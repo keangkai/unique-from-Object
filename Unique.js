bf46f05bc1c35b251edb80b9a64ceca03363dde4
Keangkai Poungmanee <m.keangkai@gmail.com>
2020-01-22 09:17:49 +0700

    Update Unique.js

--- Unique.js
+++ Unique.js
@@ -1,1 +1,13 @@
 
+const bills = [
+  { date: '2018-01-20', amount: '220', category: 'Electricity' },
+  { date: '2018-01-20', amount: '20', category: 'Gas' },
+  { date: '2018-01-20', amount: '20', category: 'Gas' },
+  { date: '2018-01-20', amount: '20', category: 'gg' },
+  { date: '2018-01-20', amount: '20', category: 'environment' },
+  { date: '2018-02-20', amount: '120', category: 'Electricity' }
+]
+
+const categories = [...new Set(bills.map(bill => bill.category))]
+
+console.log("categories :: ", categories)
